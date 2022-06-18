aa8938c39b70112b78f3fdcb1ae58d4650e9325e
Humber Sanchez Nieto <husani925@gmail.com>
2022-06-17 23:35:14 -0500

    portfolio gerson v1

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import Homepage3Light from "./pages/Homepage3Light";
+
+import Bloglist from "./pages/Bloglist";
+import BlogDetails from "./pages/BlogDetails";
+import "./App.scss";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route path="/" exact>
+          <Homepage3Light />
+        </Route>
+        <Route path="/blogs" component={Bloglist} exact />
+        <Route path="/blogs/blog-details/:id/:title" component={BlogDetails} />
+      </Switch>
+    </BrowserRouter>
+  );
+}
+
+export default App;
